2bba6e624d3dd1d582538c0dd2f0528bd3ba660e
Sophia <piercesp@outlook.com>
2025-12-23 19:01:21 +0000

    Joining datasets

--- analysis.py
+++ analysis.py
@@ -23,6 +23,7 @@ fatalities_input_df = fatalities_input_df[fatalities_input_df['FATALITY_LOCATION
 detail_df = pd.DataFrame(None)
 fatalities_df = pd.DataFrame(None)
 
+
 #~~~~~~~      DETAILS CLEANING        ~~~~~
 detail_df['event_id'] = details_input_df['EVENT_ID']
 detail_df['state'] = details_input_df['STATE']
@@ -35,12 +36,29 @@ detail_df['start_day'] = details_input_df['BEGIN_DAY']
 detail_df['injuries'] = details_input_df['INJURIES_DIRECT'] + details_input_df['INJURIES_INDIRECT']
 detail_df['deaths'] = details_input_df['DEATHS_DIRECT'] + details_input_df['DEATHS_INDIRECT']
 detail_df['fujita_scale'] = details_input_df['TOR_F_SCALE']
+detail_df.set_index('event_id', inplace = True)
+
 
 #~~~~~~~      FATALITIES CLEANING        ~~~~~
 fatalities_df['event_id'] = fatalities_input_df['EVENT_ID']
 fatalities_df['fatality_location'] = fatalities_input_df['FATALITY_LOCATION']
 
-print(fatalities_df)
+# reformatting by number of fatalities by location and event
+grouped_df = fatalities_df.groupby(['event_id', 'fatality_location']).size().reset_index(name='count')
+pivot_df = grouped_df.pivot(index='event_id', columns='fatality_location', values='count').fillna(0)
+
+
+#~~~~~~~      JOINING        ~~~~~
+joined_df = detail_df.join(pivot_df)
+
+location_cols = ['Ball Field', 'Boating', 'Business', 'Camping', 'Church', 'Golfing',
+       'Heavy Equipment/Construction', 'In Water', 'Mobile/Trailer Home',
+       'Other', 'Outside/Open Areas', 'Permanent Home', 'Permanent Structure',
+       'Under Tree', 'Vehicle/Towed Trailer']
+joined_df[location_cols] = joined_df[location_cols].fillna(0)
+
+print(joined_df)
+
 
 # machine learning
 # predict how many casualties each tornado would cause

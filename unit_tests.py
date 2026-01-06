25f6cef68a55e6cfff5ac32d6777ab7f3d593387
Sophia Pierce <piercesp@outlook.com>
2026-01-06 18:49:09 +0000

    Update unit_tests.py

--- unit_tests.py
+++ unit_tests.py
@@ -1,1 +1,21 @@
+import unittest
 
+
+class subtraction_checks(unittest.TestCase):
+    def test_unit(self):
+
+        function_output = 8 - 2
+        expected_output = 6
+        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
+
+        function_output = -24 - 5)
+        expected_output = -29
+        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
+
+        function_output = -6 - -7
+        expected_output = 1
+        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
+
+
+if __name__ == '__main__':
+    unittest.main()

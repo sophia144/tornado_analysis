88ebced3a83e32369a14ab8439b56131989aaddd
sophia144 <piercesp@outlook.com>
2026-01-07 16:51:26 +0000

    Updates unit tests to test ml classification accuracy

--- unit_tests.py
+++ unit_tests.py
@@ -1,21 +1,15 @@
 import unittest
 
+from analysis import scores
 
-class subtraction_checks(unittest.TestCase):
-    def test_unit(self):
+class circle_ci_tests(unittest.TestCase):
+    def test_ml_accuracy(self):
+        
+        function_output = scores.mean()
+        minimum_accuracy = 0.8
+        self.assertTrue(function_output > minimum_accuracy)
 
-        function_output = 8 - 2
-        expected_output = 6
-        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
-
-        function_output = -24 - 5
-        expected_output = -29
-        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
-
-        function_output = -6 - -7
-        expected_output = 1
-        self.assertEqual(function_output, expected_output, f"Fail: expected {expected_output}, got {function_output}")
-
+        
 
 if __name__ == '__main__':
     unittest.main()
